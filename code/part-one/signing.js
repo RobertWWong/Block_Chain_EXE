909e590cb9be14da6a7992f2633ea4927d65effe
Rob Wong <robwong@Mukuls-MacBook-Pro-2.local>
2018-08-21 20:14:32 -0700

    Signing tests are completed and done

---
+++ code/part-one/signing.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const secp256k1 = require('secp256k1');
+const { randomBytes, createHash } = require('crypto');
+
+
+/**
+ * A function which generates a new random Secp256k1 private key, returning
+ * it as a 64 character hexadecimal string.
+ *
+ * Example:
+ *   const privateKey = createPrivateKey();
+ *   console.log(privateKey);
+ *   // 'e291df3eede7f0c520fddbe5e9e53434ff7ef3c0894ed9d9cbcb6596f1cfe87e'
+ */
+const createPrivateKey = () => {
+  // Enter your solution here
+	let p_key;
+	do {
+		p_key = randomBytes(32);
+	} while (!secp256k1.privateKeyVerify(p_key));
+	return Buffer.from(p_key, 'hex').toString('hex');
+};
+
+
+/**
+ * A function which takes a hexadecimal private key and returns its public pair
+ * as a 66 character hexadecimal string.
+ *
+ * Example:
+ *   const publicKey = getPublicKey(privateKey);
+ *   console.log(publicKey);
+ *   // '0202694593ddc71061e622222ed400f5373cfa7ea607ce106cca3f039b0f9a0123'
+ *
+ * Hint:
+ *   Remember that the secp256k1-node library expects raw bytes (i.e Buffers),
+ *   not hex strings! You'll have to convert the private key.
+ */
+const getPublicKey = privateKey => {
+  // Your code here
+  const publicKey = secp256k1.publicKeyCreate(Buffer.from(privateKey, 'hex'));
+  // return Buffer.from(publicKey, 'hex').toString('hex');
+  return publicKey.toString('hex');
+};
+/**
+ * A function which takes a hex private key and a string message, returning
+ * a 128 character hexadecimal signature.
+ *
+ * Example:
+ *   const signature = sign(privateKey, 'Hello World!');
+ *   console.log(signature);
+ *   // '4ae1f0b20382ad628804a5a66e09cc6bdf2c83fa64f8017e98d84cc75a1a71b52...'
+ *
+ * Hint:
+ *   Remember that you need to sign a SHA-256 hash of the message,
+ *   not the message itself!
+ */
+const sign = (privateKey, message) => {
+  // Your code here
+ const buff_private = Buffer.from(privateKey, 'hex');
+ const hash = createHash('sha256');
+ hash.update(message)
+ const buff_msg = Buffer.from(hash.digest('hex'), 'hex');
+ const result = secp256k1.sign(buff_msg, buff_private);
+
+ return result['signature'].toString('hex');
+};
+
+/**
+ * A function which takes a hex public key, a string message, and a hex
+ * signature, and returns either true or false.
+ *
+ * Example:
+ *   console.log( verify(publicKey, 'Hello World!', signature) );
+ *   // true
+ *   console.log( verify(publicKey, 'Hello World?', signature) );
+ *   // false
+ */
+const verify = (publicKey, message, signature) => {
+  // Your code here
+	const buff_public = Buffer.from(publicKey, 'hex');
+	const hash = createHash('sha256');
+    hash.update(message)
+    const buff_msg = Buffer.from(hash.digest('hex'), 'hex');
+	
+	const buff_sign = Buffer.from(signature, 'hex')
+	const res = secp256k1.verify(buff_msg, buff_sign, buff_public);
+
+	return res;
+};
+
+// let pkey = createPrivateKey();
+// console.log(pkey);
+//
+// let test = randomBytes(32);
+//
+// const pubKey = getPublicKey(pkey);
+//
+// const signObj = sign(test, pkey);
+
+
+module.exports = {
+  createPrivateKey,
+  getPublicKey,
+  sign,
+  verify
+};
